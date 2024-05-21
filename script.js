12975b4a962e7f66082eaff02ae8f55c08829ce1
Ulises-85952022 <ulises.fh@gmail.com>
2024-05-20 23:36:17 -0600

    mayo2024

--- script.js
+++ script.js
@@ -1,37 +1,30 @@
-function encriptar(){
+function encriptar() {
+    var frase = document.getElementById("textoEncriptado").value.toLowerCase();
 
-    var frase =document.getElementById("textoEncriptado").value.
-    toLowerCase();
+    var textoEncriptado = frase.replace(/e/img, "enter");
+    textoEncriptado = textoEncriptado.replace(/o/img, "ober");
+    textoEncriptado = textoEncriptado.replace(/i/img, "imes");
+    textoEncriptado = textoEncriptado.replace(/a/img, "ai");
+    textoEncriptado = textoEncriptado.replace(/u/img, "ufat");
 
-    var textoEncriptado = frase.replace(/e/img,"enter");
-    var textoEncriptado = textoEncriptado.replace(/o/img,"ober");
-    var textoEncriptado = textoEncriptado.replace(/i/img,"imes");
-    var textoEncriptado = textoEncriptado.replace(/a/img,"ai");
-    var textoEncriptado = textoEncriptado.replace(/u/img,"ufat");
-
-    document.getElementById("textoDesencriptado").innerHTML =textoEncriptado;
-    document.getElementById("botonCopiar").style.display = "show";
+    document.getElementById("textoDesencriptado").innerHTML = textoEncriptado;
     document.getElementById("botonCopiar").style.display = "inherit";
-
-
 }
-function desencriptar(){
 
-    var frase =document.getElementById("textoEncriptado").value.
-    toLowerCase();
+function desencriptar() {
+    var frase = document.getElementById("textoEncriptado").value.toLowerCase();
 
-    var textoEncriptado = frase.replace(/enter/img,"e");
-    var textoEncriptado = textoEncriptado.replace(/ober/img,"o");
-    var textoEncriptado = textoEncriptado.replace(/imes/img,"i");
-    var textoEncriptado = textoEncriptado.replace(/ai/img,"a");
-    var textoEncriptado = textoEncriptado.replace(/ufat/img,"u");
+    var textoDesencriptado = frase.replace(/enter/img, "e");
+    textoDesencriptado = textoDesencriptado.replace(/ober/img, "o");
+    textoDesencriptado = textoDesencriptado.replace(/imes/img, "i");
+    textoDesencriptado = textoDesencriptado.replace(/ai/img, "a");
+    textoDesencriptado = textoDesencriptado.replace(/ufat/img, "u");
 
-    document.getElementById("textoDesencriptado").innerHTML =textoEncriptado;
+    document.getElementById("textoDesencriptado").innerHTML = textoDesencriptado;
 }
-function copiar (){
-    
-    var contenido =document.querySelector("#textoDesencriptado");
+
+function copiar() {
+    var contenido = document.querySelector("#textoDesencriptado");
     contenido.select();
     document.execCommand("copy");
 }
-//
